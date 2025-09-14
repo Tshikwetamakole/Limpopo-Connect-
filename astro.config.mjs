a9d5d0fc850192a916fb32508febe77f26691eac
Emmanuel Charley Raluswinga <105504810+Tshikwetamakole@users.noreply.github.com>
2025-09-14 16:29:50 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,3 +6,10 @@ export default defineConfig({
   site: 'https://limpopoconnect.site',
   integrations: [tailwind()]
 });
+
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://tshikwetamakole.github.io", // your GitHub username + .github.io
+  base: "/Limpopo-Connect-/",               // repo name with trailing slash
+});
