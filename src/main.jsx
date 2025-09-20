3ed80c11c981659594739e9fc0d5edb152005ea9
Emmanuel Charley Raluswinga <105504810+Tshikwetamakole@users.noreply.github.com>
2025-09-20 05:35:28 +0000

    fix(app): wrap with HelmetProvider and AuthProvider to ensure contexts and SEO work in production

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HelmetProvider>
   </StrictMode>,
 )
